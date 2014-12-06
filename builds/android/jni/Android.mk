5a42070de75083df926202430c2ea1eab57a7b5f
oleh.kulykov <oleh.kulykov@masterofcode.com>
2014-12-06 11:09:57 +0200

    Sockets new memory api

--- builds/android/jni/Android.mk
+++ builds/android/jni/Android.mk
@@ -19,6 +19,7 @@ ALL_SOURCES := \
 	../../../libwebsockets/lib/pollfd.c \
 	../../../libwebsockets/lib/service.c \
 	../../../libwebsockets/lib/sha-1.c \
+	../../../libwebsockets/lib/alloc.c \
 	../../../jansson/src/dump.c \
 	../../../jansson/src/error.c \
 	../../../jansson/src/hashtable.c \
