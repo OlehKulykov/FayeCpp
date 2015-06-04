d3704205bcb570beb1ffd5d31bf14b20cfce9614
OlehKulykov <nonamedemail@gmail.com>
2015-06-04 13:24:06 +0200

    Update copyright contact info

--- src/REBufferNoCopy.cpp
+++ src/REBufferNoCopy.cpp
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2014 - 2015 Kulykov Oleh <nonamedemail@gmail.com>
+ *   Copyright (c) 2014 - 2015 Kulykov Oleh <info@resident.name>
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
