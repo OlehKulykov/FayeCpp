d36f532e43d3c6a28d56f360fb367e2d424679da
OlehKulykov <nonamedemail@gmail.com>
2016-01-03 00:13:29 +0100

    Update licence dates

--- src/FCWebSocketQt.h
+++ src/FCWebSocketQt.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (c) 2014 - 2015 Kulykov Oleh <nonamedemail@gmail.com>
+ *   Copyright (c) 2014 - 2016 Kulykov Oleh <nonamedemail@gmail.com>
  *
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
  *   of this software and associated documentation files (the "Software"), to deal
