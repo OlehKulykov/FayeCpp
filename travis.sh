3d8b5d8fedec8154157a80d60b562f997f7c4e43
Oleh Kulykov <oleh.kulykov@boerse-go.de>
2017-03-21 15:40:54 +0100

    Travis build script

---
+++ travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ["$TRAVIS_OS_NAME" == "linux"];
+then
+	sudo apt-get -qq update
+	sudo apt-get install -y -qq cmake
+fi
+
+if ["$TRAVIS_OS_NAME" == "osx"];
+then
+	brew update
+	brew install openssl
+fi
