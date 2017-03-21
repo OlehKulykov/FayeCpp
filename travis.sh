d6213cb330cc1fb172f6ee639b3b70ef4eb1ceee
Oleh Kulykov <oleh.kulykov@boerse-go.de>
2017-03-21 15:46:19 +0100

    Travis build script

--- travis.sh
+++ travis.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if ["$TRAVIS_OS_NAME" == "linux"];
+if [ "$TRAVIS_OS_NAME" == "linux" ];
 then
 	sudo apt-get -qq update
 	sudo apt-get install -y -qq cmake
 fi
 
-if ["$TRAVIS_OS_NAME" == "osx"];
+if [ "$TRAVIS_OS_NAME" == "osx" ];
 then
 	brew update
 	brew install openssl
